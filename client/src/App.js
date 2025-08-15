ad8a261a34cc9a0897a1d30f61c178b4ed644204
shadowmoonezios <agushifelde@hotmail.com>
2025-08-15 14:52:08 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meal Planner App</h1>
+      <p>Plane deine Mahlzeiten hier!</p>
+    </div>
+  );
+}
+
+export default App;
